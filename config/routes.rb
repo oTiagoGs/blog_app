c28773a14b61a8fdc160e24d6bcf0c1a7e6598c6
oTiagoGs <tiagoguimaraes81@gmail.com>
2024-04-05 16:25:16 -0300

    Configurando a rota padrão para a página principal

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   namespace :site do
     get 'welcome/index'
   end
+
+  root to: 'site/welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
