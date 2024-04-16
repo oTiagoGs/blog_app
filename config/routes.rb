950badb72e8eeabe365d64341b144c40b1f16897
oTiagoGs <tiagoguimaraes81@gmail.com>
2024-04-16 14:58:56 -0300

    Ajustando a rota dos admins

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
 
   devise_for :admins
+  
   namespace :admins_backoffice do
-    get 'welcome/index'
-    get 'admins/index'
+    get 'welcome/index' #Dashboard
+    resources :admins, only: [:index]
   end
   namespace :site do
     get 'welcome/index'
