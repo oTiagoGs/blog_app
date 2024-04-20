34d95af44f2d5b6f561ab57155d5ffde91ff7c3d
oTiagoGs <tiagoguimaraes81@gmail.com>
2024-04-19 21:49:29 -0300

    Usando paginação com kaminari

--- app/controllers/admins_backoffice/admins_controller.rb
+++ app/controllers/admins_backoffice/admins_controller.rb
@@ -3,7 +3,7 @@ class AdminsBackoffice::AdminsController < AdminsController
   before_action :verify_password, only: [:update]
 
   def index
-    @admins = Admin.all
+    @admins = Admin.all.page(params[:page])
   end
 
   def new
