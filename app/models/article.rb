b754e2d555c9405265a899d426990bdb13e6cba4
oTiagoGs <tiagoguimaraes81@gmail.com>
2024-05-03 12:44:46 -0300

    Fazendo algumas validações

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,6 +2,10 @@ class Article < ApplicationRecord
   belongs_to :subject
   belongs_to :admin
 
+  # Validations
+  validates :title, :content, presence: true
+
+  # Pagination
   paginates_per 5
   
 end
